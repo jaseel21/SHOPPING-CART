fe59f14362a9b99e75a1ff12d2ba37c104f4336a
Thashfeeq jaseel <thashfeeq123@gmail.com>
2021-06-10 14:47:38 +0530

    thired step is completed

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,7 @@
 var express = require('express');
+const productHelpers = require('../helpers/product-helpers');
 var router = express.Router();
+var productHelper=require('../helpers/product-helpers')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -34,8 +36,17 @@ router.get('/add-product',function(req,res){
   res.render('admin/add-product')
 })
 router.post('/add-product',(req,res)=>{
-  console.log(req.body);
-  console.log(req.files);
+  
+  productHelpers.addProduct(req.body,(id)=>{
+    let image = req.files.Image
+    image.mv('./public/product-images/'+id+'.jpg',(err)=>{
+      if(!err){
+        res.render('admin/add-product')
+      } else{
+        console.log(err);
+      }
+    })
+  })
   
 })
 
