2db0057c0bb24a6139a3c7045d1023442b045be1
Thashfeeq jaseel <thashfeeq123@gmail.com>
2021-06-10 20:22:44 +0530

    fourth step is completed

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,8 +1,15 @@
 var db = require('../config/connection')
+var collection=require('../config/collections')
 module.exports={
     addProduct:(product,callback)=>{
         db.get().collection('product').insertOne(product).then((data)=>{
             callback(data.ops[0]._id)
+        })
+    },
+    getAllProducts:()=>{
+        return new Promise((resolve,reject)=>{
+            let products=db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
+            resolve(products)
         })
     }
 }
